fb7bcf3167c05e3b508bcfceb264ffba681c6197
Jason Garber <jgarber@appcove.com>
2022-02-07 00:20:29 -0500

    Added egui-144-ball-chaser-2-player

--- examples/egui-144-ball-chaser-2-player/src/main.rs
+++ examples/egui-144-ball-chaser-2-player/src/main.rs
@@ -1,17 +1,28 @@
-use std::ops::Add;
-
+use rand;
 use eframe::epi;
 use eframe::egui;   
 use egui::Color32;  //for circle
 use egui::Stroke;   //for cricle
 use egui::Key;
 use egui::Pos2;
-use egui::Vec2;
+//use egui::Vec2;
 
 const ACCEL_PER_FRAME: f32 = 0.1;
 const ANGLE_PER_FRAME: f32 = 0.1;
 const MAX_SPEED: f32 = 8.0;
-const WHITE_STROKE: Stroke = Stroke{width: 2.0, color: Color32::from_rgb(255, 255, 255)};
+const WHITE_STROKE: Stroke = Stroke{width: 2.0, color: Color32::WHITE};
+//const NO_STROKE: Stroke = Stroke{width: 0.0, color: Color32::TRANSPARENT};
+
+struct Obsticle {
+    pos: Pos2,
+    radius: f32,
+}
+
+impl Obsticle {
+    fn paint(&self, painter: &egui::Painter) {
+        painter.circle_filled(self.pos, self.radius, Color32::DARK_GRAY);
+    }
+}
 
 struct Player {
     x: f32,
@@ -31,9 +42,7 @@ impl Player {
         else if speed < -MAX_SPEED {
             speed = -MAX_SPEED;
         }
-        else {
-            self.speed = speed;
-        }
+        self.speed = speed;
     }
 
     fn inc_speed(&mut self, increment: f32) {
@@ -47,6 +56,8 @@ impl Player {
     fn tick(&mut self) {
         self.x += self.speed * self.angle.cos();
         self.y += self.speed * self.angle.sin();
+
+
     }
 
     fn get_pos(&self) -> Pos2 {
@@ -54,9 +65,8 @@ impl Player {
     }
 
     fn paint(&self, painter: &egui::Painter) {
-        let nosex = self.x + self.radius * self.angle.cos();
-        let nosey = self.y + self.radius * self.angle.sin();
-
+        let nose = Pos2{x: self.x + self.radius * self.angle.cos(), y: self.y + self.radius * self.angle.sin()};
+        
 
         painter.circle(
             self.get_pos(), 
@@ -64,33 +74,43 @@ impl Player {
             self.color, 
             WHITE_STROKE
         );
-        painter.circle(
-            Pos2 {x:nosex, y:nosey},
-            self.radius / 5.0, 
-            Color32::BLACK, 
-            WHITE_STROKE
-        );
+        painter.line_segment([self.get_pos(), nose], WHITE_STROKE);
 
     }
 }
 
 
 struct ExampleApp {
+    paused: bool,
     player1: Player,
     player2: Player,   
+    obstacles: Vec<Obsticle>,
 }
 
 
 impl Default for ExampleApp {
     fn default() -> Self {
+        let mut obstacles = Vec::new();
+        // Create 100 random obstacles
+        for _ in 0..100 {
+            let radius = (rand::random::<f32>() * 40.0) + 40.0;
+            let x = (rand::random::<f32>() * 4000.0) - radius;
+            let y = (rand::random::<f32>() * 2000.0) - radius;
+            if x > 300.0 || y > 300.0 {
+               obstacles.push(Obsticle{pos: Pos2{x, y}, radius});
+            }
+        }
+        
+
         Self {
+            paused: false,
             player1: Player { 
                 x: 100.0, 
                 y: 100.0, 
                 angle: 0.0,
                 speed: 0.0,
                 radius: 20.0, 
-                color: Color32::LIGHT_BLUE, 
+                color: Color32::DARK_GREEN, 
             },
             player2: Player { 
                 x: 200.0, 
@@ -98,9 +118,16 @@ impl Default for ExampleApp {
                 angle: 0.0,
                 speed: 0.0,
                 radius: 20.0, 
-                color: Color32::LIGHT_YELLOW, 
+                color: Color32::DARK_BLUE, 
             },
+            obstacles: obstacles,
         }
+    }
+}
+
+impl ExampleApp {
+    fn reset(&mut self) {
+        *self = Self::default();
     }
 }
 
@@ -115,6 +142,10 @@ impl epi::App for ExampleApp {
         ctx.set_pixels_per_point(1.5);
 
         let instate = &ctx.input();
+
+        if instate.key_pressed(Key::Space) {
+            self.paused = !self.paused;
+        }
 
         if instate.key_down(Key::W) {
             self.player1.inc_speed(ACCEL_PER_FRAME);
@@ -148,23 +179,56 @@ impl epi::App for ExampleApp {
             self.player2.inc_angle(ANGLE_PER_FRAME);
         }
 
-        self.player1.tick();        
-        self.player2.tick();
+        if !self.paused {
+            self.player1.tick();
+            self.player2.tick();
 
+            
+            for obsticle in self.obstacles.iter() {
+                // if the distance between self and obsticle is less than the sum of the radii
+                if (self.player1.x - obsticle.pos.x).powi(2) + (self.player1.y - obsticle.pos.y).powi(2) < (self.player1.radius + obsticle.radius).powi(2) {
+                    self.player1.speed = 0.0;
+                }
+                if (self.player2.x - obsticle.pos.x).powi(2) + (self.player2.y - obsticle.pos.y).powi(2) < (self.player2.radius + obsticle.radius).powi(2) {
+                    self.player2.speed = 0.0;
+                }
+            }
+
+            // check player1 and player2 for collision
+            if (self.player1.x - self.player2.x).powi(2) + (self.player1.y - self.player2.y).powi(2) < (self.player1.radius + self.player2.radius).powi(2) {
+                self.reset();
+            }
+    
+
+
+        }
+       
         egui::CentralPanel::default().show(ctx, |ui| {
-            if ui.button("Quit").clicked() {
-                frame.quit()
-            };
-            if ui.button("Reset").clicked() {
-                self.player1.x = 100.0;
-                self.player1.y = 100.0;
-                self.player2.x = 200.0;
-                self.player2.y = 200.0;
-                self.player2.speed = 0.0;
-                self.player1.speed = 0.0;
-            };
+            ui.horizontal(|ui| {
+                if self.paused {
+                    if ui.button("Resume").clicked() {
+                        self.paused = false;
+                    };
+                }
+                else {
+                    if ui.button("Pause").clicked() {
+                        self.paused = true;
+                    };
+                }
+                if ui.button("Reset").clicked() {
+                    self.reset();
+                };
+                if ui.button("Quit").clicked() {
+                    frame.quit()
+                };
+            });
 
             let painter = ui.painter();
+
+            for obsticle in &self.obstacles {
+                obsticle.paint(painter);
+            }
+
             self.player1.paint(painter);
             self.player2.paint(painter);
 
