50a3e62183599637d6329d4962ce58f159f7844e
Timothy Garber <timothy@appcove.com>
2022-03-24 10:13:30 -0400

    Finished 3 exmaples Multiple-choice, Text-size, and Combo-box.

---
+++ examples/egui-122-combo-box/README.md
@@ -0,0 +1,20 @@
+
+# egui-133-context-menus
+
+A basic display of how egui context menus work.
+
+## Source
+- [src/main.rs]({{ site.codeurl }}/examples/egui-133-context-menus/src/main.rs)
+- [Project Directory]({{ site.codeurl }}/examples/egui-133-context-menus)
+
+
+## Screenshots
+
+![Screenshot](screen1.png)
+
+## Description
+
+This project has multiple buttons, if you interact in a certain way with them, they will show context menus which can lead to more context menus.
+
+
+
---
+++ examples/egui-122-text-size/src/main.rs
@@ -0,0 +1,80 @@
+use eframe::epi;
+use eframe::egui;
+
+struct ExampleApp {
+    selected: i32
+}
+
+impl Default for ExampleApp {
+    fn default() -> Self {
+        Self {
+           selected: 1
+        }
+    }
+}
+
+impl epi::App for ExampleApp {
+    fn name(&self) -> &str {
+        "egui-122-text"
+    }
+    
+    fn update(&mut self, ctx: &egui::CtxRef, _frame: &epi::Frame) {
+        ctx.set_pixels_per_point(1.5); 
+        ctx.request_repaint();
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                if ui.button("Quit").clicked() {
+                    _frame.quit()
+                };
+                egui::ComboBox::from_label("")
+                    .selected_text("Text Size")
+                    .show_ui(ui, |ui| {
+                        ui.selectable_value(&mut self.selected, 1, "Big");
+                        ui.selectable_value(&mut self.selected, 2, "Medium");
+                        ui.selectable_value(&mut self.selected, 3, "Small");
+                    }
+                );
+            });
+        });
+        egui::CentralPanel::default().show(ctx, |ui| {   
+            
+            let mut fonts = egui::FontDefinitions::default();
+            // Large button text:
+            if self.selected == 1 {
+                fonts.family_and_size.insert(
+                    egui::TextStyle::Body,
+                    (egui::FontFamily::Proportional, 20.0));
+                ctx.set_fonts(fonts);
+            } else if self.selected == 2 {
+                fonts.family_and_size.insert(
+                    egui::TextStyle::Body,
+                    (egui::FontFamily::Proportional, 15.0));
+                ctx.set_fonts(fonts);
+            } else {
+                fonts.family_and_size.insert(
+                    egui::TextStyle::Body,
+                    (egui::FontFamily::Proportional, 10.0));
+                ctx.set_fonts(fonts);
+            }
+            ui.heading("Text here:");
+            ui.heading("");
+            ui.label("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+            ui.label("tempor incididunt ut labore et dolore magna aliqua. Semper risus in");
+            ui.label("hendrerit gravida rutrum quisque non. Tempus imperdiet nulla");
+            ui.label("malesuada pellentesque elit. Nec dui nunc mattis enim ut tellus");
+            ui.label("elementum sagittis. Metus aliquam eleifend mi in.");
+        });
+        
+    }
+}
+
+fn main() {
+    let app = ExampleApp::default();
+    
+    let native_options = eframe::NativeOptions{
+        initial_window_size: Some(egui::Vec2{x: 400.0, y: 400.0}),
+        ..eframe::NativeOptions::default()
+    };
+
+    eframe::run_native(Box::new(app), native_options);
+}
