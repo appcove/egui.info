cc067cc276844a3f37aaf6ba34debe5abd9eaac7
Ilay Ron <ilayron01@gmail.com>
2023-11-25 12:13:03 +0100

    Upgrade egui-144-circle-chaser to eframe 0.24

--- examples/egui-144-circle-chaser/src/main.rs
+++ examples/egui-144-circle-chaser/src/main.rs
@@ -1,8 +1,9 @@
-use eframe::epi;
 use eframe::egui;
+//for circle
+use egui::Color32;
+use egui::Stroke;
+
 use rand::Rng;
-use egui::Color32;  //for circle
-use egui::Stroke;   //for cricle
 
 struct ExampleApp {
     cx: f32,
@@ -15,6 +16,12 @@ struct ExampleApp {
     ty: f32,
     ts: f32,
     dd: f32,
+}
+
+impl ExampleApp {
+    fn name() -> &'static str {
+        "egui-144-circle-chaser"
+    }
 }
 
 impl Default for ExampleApp {
@@ -34,41 +41,36 @@ impl Default for ExampleApp {
     }
 }
 
-
-
-
-impl epi::App for ExampleApp {
-    fn name(&self) -> &str {
-        "egui-144-circle-chaser"
-    }
-    
-    fn update(&mut self, ctx: &egui::CtxRef, frame: &epi::Frame) {
-        
-
-
+impl eframe::App for ExampleApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // Looks better on 4k montior
         ctx.set_pixels_per_point(1.5);
         self.dd = self.cs + self.ts;
 
-        if (egui::Pos2{x:self.cx,y:self.cy}).distance(egui::Pos2{x:self.tx,y:self.ty}) < self.dd {
+        if (egui::Pos2 {
+            x: self.cx,
+            y: self.cy,
+        })
+        .distance(egui::Pos2 {
+            x: self.tx,
+            y: self.ty,
+        }) < self.dd
+        {
             self.tx = rand::thread_rng().gen_range(0.0..1000.0);
-            self.ty = rand::thread_rng().gen_range(0.0..1000.0);    
+            self.ty = rand::thread_rng().gen_range(0.0..1000.0);
         }
-
 
         self.cx += self.sx;
         self.cy += self.sy;
 
         if self.sx > 0.00 {
             self.sx -= 0.01
-        }
-        else if self.sx < 0.00 {
+        } else if self.sx < 0.00 {
             self.sx += 0.01
         }
         if self.sy > 0.00 {
             self.sy -= 0.01
-        }
-        else if self.sy < 0.00 {
+        } else if self.sy < 0.00 {
             self.sy += 0.01
         }
 
@@ -85,49 +87,59 @@ impl epi::App for ExampleApp {
             self.sy = 3.0
         }
 
-
-        
-        if ctx.input().key_down(egui::Key::W) {
-            self.sy += -0.2
-        }
-        if ctx.input().key_down(egui::Key::S) {
-            self.sy += 0.2
-        }
-        if ctx.input().key_down(egui::Key::A) {
-            self.sx += -0.2
-        }
-        if ctx.input().key_down(egui::Key::D) {
-            self.sx += 0.2
-        }
-        if ctx.input().key_down(egui::Key::ArrowUp){
-            self.cs += 5.0
-        }
-        if ctx.input().key_down(egui::Key::ArrowDown){
-            self.cs += -5.0
-        }
-        if ctx.input().key_down(egui::Key::O){
-            self.cc = Color32::from_rgb(215, 100, 000)
-        }
-        if ctx.input().key_down(egui::Key::C){
-            self.cc = Color32::from_rgb(000, 180, 215)
-        }
-        
+        ctx.input(|input| {
+            if input.key_down(egui::Key::W) {
+                self.sy += -0.2
+            }
+            if input.key_down(egui::Key::S) {
+                self.sy += 0.2
+            }
+            if input.key_down(egui::Key::A) {
+                self.sx += -0.2
+            }
+            if input.key_down(egui::Key::D) {
+                self.sx += 0.2
+            }
+            if input.key_down(egui::Key::ArrowUp) {
+                self.cs += 5.0
+            }
+            if input.key_down(egui::Key::ArrowDown) {
+                self.cs += -5.0
+            }
+            if input.key_down(egui::Key::O) {
+                self.cc = Color32::from_rgb(215, 100, 000)
+            }
+            if input.key_down(egui::Key::C) {
+                self.cc = Color32::from_rgb(000, 180, 215)
+            }
+        });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-
             let painter = ui.painter();
 
             painter.circle(
-                egui::Pos2{x:self.cx,y:self.cy}, 
-                self.cs, 
-                self.cc, 
-                Stroke{width: 2.0, color: Color32::from_rgb(255, 255, 255)}
+                egui::Pos2 {
+                    x: self.cx,
+                    y: self.cy,
+                },
+                self.cs,
+                self.cc,
+                Stroke {
+                    width: 2.0,
+                    color: Color32::from_rgb(255, 255, 255),
+                },
             );
             painter.circle(
-                egui::Pos2{x:self.tx,y:self.ty}, 
-                self.ts, 
-                self.cc, 
-                Stroke{width: 2.0, color: Color32::from_rgb(255, 255, 255)}
+                egui::Pos2 {
+                    x: self.tx,
+                    y: self.ty,
+                },
+                self.ts,
+                self.cc,
+                Stroke {
+                    width: 2.0,
+                    color: Color32::from_rgb(255, 255, 255),
+                },
             );
 
             ui.monospace("Use W,A,S,D to move around.");
@@ -135,7 +147,7 @@ impl epi::App for ExampleApp {
             ui.monospace("Use Up and Down arrow keys to change size.");
 
             if ui.button("Quit").clicked() {
-                frame.quit()
+                std::process::exit(0);
             };
             if ui.button("Home").clicked() {
                 self.cx = 250.0;
@@ -152,13 +164,15 @@ impl epi::App for ExampleApp {
     }
 }
 
-fn main() {
-    let app = ExampleApp::default();
-
-    let native_options = eframe::NativeOptions{
-        initial_window_size: Some(egui::Vec2{x: 1700.0, y: 1700.0}),
+fn main() -> eframe::Result<()> {
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size((1700.0, 1700.0)),
         ..eframe::NativeOptions::default()
     };
 
-    eframe::run_native(Box::new(app), native_options);
+    eframe::run_native(
+        ExampleApp::name(),
+        native_options,
+        Box::new(|_ctx| Box::<ExampleApp>::default()),
+    )
 }
